41a1772df80d64df2289765959ef47f165620cb5
kaylynmmonk <kaylyn.monk@gmail.com>
2015-05-11 19:18:36 -0600

    W5-2 Exercise 1

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,10 @@
+helpers do
+    def current_user
+    @current_user = User.find_by(id: session[:user_id]) if session[:user_id]
+end
+
+end
+
 # Homepage (Root path)
 get '/' do
   erb :index
@@ -8,8 +15,38 @@ get '/login' do
 end
 
 post '/login' do
+  username = params[:username]
+  password = params[:password]
+
+  user = User.find_by(username: username)
+  if user.password == password
+    session[:user_id] = user.id
     redirect '/'
+  else
+    redirect '/login'
+  end
+end   
+    
+
+get '/signup' do
+    erb :signup
 end
+
+
+post '/signup' do
+  username = params[:username]
+  password = params[:password]
+
+  user = User.find_by(username: username)
+  if user
+    redirect '/signup'
+  else
+    user = User.create(username: username, password: password)
+    session[:user_id] = user.id
+    redirect '/'
+  end
+end
+
 
 
 get '/profile' do
